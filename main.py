4454e02ddcfdccfbc04eb38d54904c42d54f4022
Guilherme Henrique Dias <guilhermedais91@gmail.com>
2023-08-09 21:26:46 -0300

    Criando um bot

---
+++ main.py
@@ -0,0 +1,22 @@
+import pyautogui
+from time import sleep
+
+pyautogui.PAUSE = 2
+
+pyautogui.press("win")
+pyautogui.write("bloco de notas")
+pyautogui.press("backspace")
+
+pyautogui.press("enter")
+
+import time
+
+time.sleep(1)
+
+pyautogui.click (204,252,duration=2)
+pyautogui.write("Gustavo.H")
+
+
+
+
+
